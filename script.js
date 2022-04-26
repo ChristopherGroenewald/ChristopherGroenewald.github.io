eb129638130a4284f78ce9e1d5edfedb788d7e9f
ChristopherGroenewald <christopher.groenewald4@gmail.com>
2022-04-26 21:20:50 +0200

    Update

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-var isGood = false;
-var isGoodText = "False";
-
-function isGoodProgrammer() {
-    isGood = !isGood;
-     return isGood;
+var num = 0;
+function Incrementor() 
+{
+    num++;
+    var numValue = `<h1> ${num} </h1>`;
+    document.getElementById("result").innerHTML = numValue;
+    return num;
 }
+document.querySelector("button").addEventListener("click", Incrementor);
+
+//document.querySelector("button").onclick = Incrementor();
 
 
